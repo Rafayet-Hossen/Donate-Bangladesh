8e9437abc8dad70e6dec69f907c68ea71743a651
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-09-23 01:28:33 +0600

    Added funtionality for donation and solve it using repeated function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,62 @@
-document.getElementById('btnDonation1').addEventListener('click',function(event){
+document
+  .getElementById("btnDonation1")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    getFieldValueById('donation1');
-})
+    const fieldValue = getFieldValueById("donation1");
+
+    const accountBalance = getTextValue("main-balance");
+    const donationBalance = getTextValue("donationBalance1");
+
+    const updateBalance = updatedBalance(fieldValue, accountBalance);
+    const updateDonationBalance = updatedDonationBalance(
+      donationBalance,
+      fieldValue,
+      accountBalance
+    );
+
+    document.getElementById("main-balance").innerText = updateBalance;
+    document.getElementById("donationBalance1").innerText =
+      updateDonationBalance;
+  });
+
+document
+  .getElementById("btnDonation2")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const fieldValue = getFieldValueById("donation2");
+
+    const accountBalance = getTextValue("main-balance");
+    const donationBalance = getTextValue("donationBalance2");
+
+    const updateBalance = updatedBalance(fieldValue, accountBalance);
+    const updateDonationBalance = updatedDonationBalance(
+      donationBalance,
+      fieldValue,
+      accountBalance
+    );
+
+    document.getElementById("main-balance").innerText = updateBalance;
+    document.getElementById("donationBalance2").innerText =
+      updateDonationBalance;
+  });
+
+document
+  .getElementById("btnDonation3")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const fieldValue = getFieldValueById("donation3");
+
+    const accountBalance = getTextValue("main-balance");
+    const donationBalance = getTextValue("donationBalance3");
+
+    const updateBalance = updatedBalance(fieldValue, accountBalance);
+    const updateDonationBalance = updatedDonationBalance(
+      donationBalance,
+      fieldValue,
+      accountBalance
+    );
+
+    document.getElementById("main-balance").innerText = updateBalance;
+    document.getElementById("donationBalance3").innerText =
+      updateDonationBalance;
+  });
--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,35 @@
 function getFieldValueById(id) {
     const fieldValue = document.getElementById(id).value;
     const value = Number(fieldValue);
-    console.log(typeof value,value);
+    return value;
+}
+function getTextValue(id) {
+    const textValue = document.getElementById(id).innerText;
+    const value = Number(textValue);
+    return value;
+}
+// This function will calculate the value and return update value
+
+function updatedBalance(donate,balance) {
+    if(donate < 0) {
+        alert('Invalid Amount');
+        return balance;
+    }
+    else if(donate > balance) {
+        alert('Insufficient Balance');
+        return balance;
+    }
+    else {
+        return balance - donate;
+    }
+
+}
+
+// This will update donation balance
+
+function updatedDonationBalance(currentAmount,donation,balance) {
+    if(donation < 0 || donation > balance) {
+        return currentAmount;
+    }
+    return currentAmount + donation;
 }
